34272f61f8438fd71ab494cb692d1958e66d3fae
vikash-kumar-shah <62929700+vikash-kumar-shah@users.noreply.github.com>
2024-03-14 19:50:53 +0530

    Update db.js
    added URI

--- Backend/db.js
+++ Backend/db.js
@@ -1,6 +1,6 @@
 const mongoose=require('mongoose')
-const URI= "mongodb://localhost:27017/cloud_note_book"
-// const URI= "mongodb+srv://cybsecured36:thisisfakeaccount@cloudnb.ea8n47f.mongodb.net/?retryWrites=true&w=majority&appName=CloudNB"
+// const URI= "mongodb://localhost:27017/cloud_note_book"
+const URI= "mongodb+srv://cybsecured36:thisisfakeaccount@cloudnb.ea8n47f.mongodb.net/?retryWrites=true&w=majority&appName=CloudNB"
 const connectToMongo =async ()=>{
     try {
         await mongoose.connect(URI)
